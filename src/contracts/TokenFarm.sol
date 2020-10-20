5998c36a7d474cfca45b7330b0182cefd46cea7e
ceress <markmesina69@gmail.com>
2020-10-20 12:40:54 -0700

    init unstake balance func

--- src/contracts/TokenFarm.sol
+++ src/contracts/TokenFarm.sol
@@ -20,7 +20,7 @@ contract TokenFarm {
     dappToken = _dappToken; //pass down smart contract
     daiToken = _daiToken;
     owner = msg.sender; // assign owner
-  }
+  };
 
   // stake tokens (investor deposits)
   function stakeTokens(uint _amount) public {
@@ -36,13 +36,12 @@ contract TokenFarm {
     // add user to stakers array if they haven't
     if(!hasStaked[msg.sender]) {
       stakers.push(msg.sender);
-    }
+    };
 
     //update staking status
     isStaking[msg.sender] = true;
     hasStaked[msg.sender] = true;
-
-  }
+  };
 
   //issuing tokens (interest)
   function issueTokens() public {
@@ -53,15 +52,28 @@ contract TokenFarm {
       address recipient = stakers[i];
       uint balance = stakingBalance[recipient]; //fetch balance
 
-      if(balance > 0) {
+      if (balance > 0) {
         dappToken.transfer(recipient, balance); // 1 to 1 trade. 1mDai = 1DappToken
-      }
-    }
-  }
-
-
-
-
+      };
+    };
+  };
 
   //unstaking tokens (withdraw)
-}
+  function unstakeTokens() public {
+    //fetch balance
+    uint balance = stakingBalance[msg.sender];
+
+    // require amt greater than 0
+    require(balance > 0, 'staking balance cannot be 0');
+
+    // transfer mDAi token back to investor
+    daiToken.transfer(msg.sender, balance);
+
+    //reset staking balance
+    stakingBalance[msg.sender] = 0;
+
+    //update staking status
+    isStaking[msg.sender] = false;
+
+  };
+};
